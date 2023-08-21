298c8deb2058022472d150f5c1967bdb4b6585c0
MASH679 <aaleali666@gmail.com>
2023-08-21 16:43:06 +0500

    app done

--- lib/news_api.dart
+++ lib/news_api.dart
@@ -5,15 +5,20 @@ import 'news_model.dart';
 class NewsApi {
   static Future<List<NewsArticle>> fetchArticles() async {
     final apiKey = 'fce64fb7068c4fa7b215d47b68ed1915';
-    final response = await http.get(Uri.parse('/v2/everything'));
+    final endpoint = '/v2/everything?q=bitcoin&apiKey=$apiKey';
+    final baseUrl = 'https://newsapi.org';
+
+    final response = await http.get(Uri.parse('$baseUrl$endpoint'));
 
     if (response.statusCode == 200) {
-      final List<dynamic> jsonData = json.decode(response.body);
-      return jsonData
+      final Map<String, dynamic> jsonData = json.decode(response.body);
+      final List<dynamic> articlesData = jsonData['articles'];
+
+      return articlesData
           .map((article) => NewsArticle(
                 title: article['title'],
                 description: article['description'],
-                imageUrl: article['imageUrl'],
+                imageUrl: article['urlToImage'],
               ))
           .toList();
     } else {
